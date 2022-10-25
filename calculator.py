d7f9bb6fc36017202d0d68933842d233198a099e
grouchyspanky <82906760+grouchyspanky@users.noreply.github.com>
2022-10-25 16:29:15 -0700

    cont

---
+++ calculator.py
@@ -0,0 +1,13 @@
+'first Calculator in Python'
+
+
+
+print('Welcome to my first Python Calculator\n')
+print('Enter in two numbers and then choose which artithmetic to perform\n')
+
+userNum1 = input('Enter in the first value\n')
+userNum2 = input('Enter in the second value\n')
+
+userChoice = input("Enter in the Arithmetic you wish to perform\n")
+
+if 
---
+++ helloworld.py
@@ -0,0 +1,5 @@
+"First print / input test for Python"
+
+name = input("What is your cats names?")
+
+print('Hello Cats' + name)

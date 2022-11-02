0ee84fbc83af2828b2ffb9ed435f813b197fceb0
grouchyspanky <82906760+grouchyspanky@users.noreply.github.com>
2022-11-01 23:38:44 -0700

    test

--- Excercises/set.py
+++ Excercises/set.py
@@ -1,2 +1,19 @@
 'working w/ sets'
 
+my_list=[1,2,3,4,5]
+
+my_set = {1,2,3,4,5,5}
+
+new_set=my_set.copy
+
+my_set.clear()
+
+print(my_set)
+
+print(new_set)
+
+print(5 in my_set)
+
+print(set(my_list))
+
+print(my_set)

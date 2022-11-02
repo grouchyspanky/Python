cb8a6c3e0f37e612da1ac388c8bf2c04de8493ed
grouchyspanky <82906760+grouchyspanky@users.noreply.github.com>
2022-11-02 12:07:17 -0700

    conditions

---
+++ Excercises/conditions.py
@@ -0,0 +1,11 @@
+'working with conditions'
+is_old = True
+
+is_licenced = True
+
+if is_old and is_licenced:
+    print("You are old enough to drive")
+elif is_licenced:
+    print('You can have no license')    
+else:
+    print("You are not old enough")

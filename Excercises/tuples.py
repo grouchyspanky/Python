80fb72cdc5db022451535e1045b98d8ff83d2dd3
grouchyspanky <82906760+grouchyspanky@users.noreply.github.com>
2022-11-01 23:29:44 -0700

    test

--- Excercises/tuples.py
+++ Excercises/tuples.py
@@ -2,12 +2,20 @@
 
 my_tuple=(1,2,3,4,5)
 
+new_tuple=my_tuple[1:3]
+
+x,y,z, *other = (1,2,3,4,5)
+
+print(other)
+
 dict = {
     (1,2): ['meow']
 }
 
-
+print(new_tuple)
 
 print(dict[(1,2)])
 
 print(my_tuple)
+
+print(my_tuple.index(4))

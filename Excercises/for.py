5583775fe33d106ea4c70abb495fd65c28fb5d96
grouchyspanky <82906760+grouchyspanky@users.noreply.github.com>
2022-11-08 13:52:55 -0800

    test

---
+++ Excercises/for.py
@@ -0,0 +1,16 @@
+'working with lists'
+
+secrets = {
+    'name': 'meow',
+    'age': '20',
+    'eats': 'yes',
+}
+
+
+
+for item in secrets.keys():
+    print(item)
+
+for item in {1,2,3,4,5}:
+    print(item)
+    

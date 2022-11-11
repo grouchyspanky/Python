8f670d9000e973ac6d5dbb82fc5ae8ac9ac63db2
grouchyspanky <82906760+grouchyspanky@users.noreply.github.com>
2022-11-10 21:27:43 -0800

    test

--- Excercises/project2.py
+++ Excercises/project2.py
@@ -6,3 +6,12 @@ weight = input ("Enter your weight in kg:\n")
 user_BMI =(float(weight) / (float(height) * float(height)))
 
 print(int(user_BMI))
+
+
+user_Num= input("Enter in a number\n")
+
+num_1=user_Num[0]
+
+num_2=user_Num[1]
+
+print(int(num_1) + int(num_2))

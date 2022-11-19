1dd2777c5bfbd3709fde8adc2090f56a9b19af5d
grouchyspanky <82906760+grouchyspanky@users.noreply.github.com>
2022-11-18 19:39:17 -0800

    test

---
+++ Excercises/project6.py
@@ -0,0 +1,12 @@
+'random functions'
+import random
+
+random_val = random.randint(1,3)
+if random_val == 1:
+    cat_name="Meow Meow"
+elif random_val ==2:
+    cat_name ="Kitties"
+elif random_val ==3:
+    cat_name ="Pumpkin"
+    
+print(cat_name)

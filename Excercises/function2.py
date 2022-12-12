b82df6f119b381cb4e9ffd9e836c6e378ed881d1
grouchyspanky <82906760+grouchyspanky@users.noreply.github.com>
2022-12-12 15:05:38 -0800

    test

--- Excercises/function2.py
+++ Excercises/function2.py
@@ -1,6 +1,7 @@
 'part two of functions'
 
-def cat_names(name1,name2):
+def cat_names(name1="Pumpkin",name2="Abby"):
     print(f'hello {name1} and {name2}')
     
+cat_names()
 cat_names(name2 = 'kitties', name1="Meow Meow")

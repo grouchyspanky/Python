fe37fa83e644ec6d90127f6753824925521a77be
grouchyspanky <82906760+grouchyspanky@users.noreply.github.com>
2022-12-16 13:00:39 -0800

    test

---
+++ Excercises/oddoreven.py
@@ -0,0 +1,9 @@
+'check odd or even using functions'
+
+def is_odd_or_even(num):
+    if num % 2 == 1:
+        print(f"{num} is odd")
+    else:
+        print(f"{num} is even")
+        
+is_odd_or_even(1113)

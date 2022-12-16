3072482e49b5d4a082d6e05fc9a0a292733c2891
grouchyspanky <82906760+grouchyspanky@users.noreply.github.com>
2022-12-16 12:54:56 -0800

    test

--- Excercises/returns.py
+++ Excercises/returns.py
@@ -1,8 +1,13 @@
 "working with returns"
 def sumnum(num1,num2):
+    '''
+    Info: this function adds two nums together
+    '''
     num3 = num1 + num2
     return num3
 
+help(sumnum)
 
+print(sumnum.__doc__)
 
 print(sumnum(5,5))   
